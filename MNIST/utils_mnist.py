c0aad6eae6c92b96b639f7f3327924d3f746f420
Simon <2053104031@qq.com>
2024-04-29 01:40:49 +1000

    Update utils_mnist.py

--- MNIST/utils_mnist.py
+++ MNIST/utils_mnist.py
@@ -2,6 +2,11 @@ import torch
 import torch.nn as nn
 
 ngpu = 1
+nc = 1
+nz = 100
+ngf = 64
+ndf = 64
+num_classes = 10
 device = torch.device('cuda:0' if (torch.cuda.is_available() and ngpu > 0) else 'cpu')
 
 
@@ -26,6 +31,142 @@ class CNN(nn.Module):
         x = self.fc2(x)
         return x
 
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.image = nn.Sequential(
+            # state size. (nz) x 1 x 1
+            nn.ConvTranspose2d(nz, ngf * 4, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True)
+            # state size. (ngf*4) x 4 x 4
+        )
+        self.label = nn.Sequential(
+            # state size. (num_classes) x 1 x 1
+            nn.ConvTranspose2d(num_classes, ngf * 4, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True)
+            # state size. (ngf*4) x 4 x 4
+        )
+
+        self.main = nn.Sequential(
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 8, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf*2, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 32 x 32
+        )
+    def forward(self, image, label):
+        image = self.image(image)
+        label = self.label(label)
+        incat = torch.cat((image, label), dim=1)
+        return self.main(incat)
+
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.image = nn.Sequential(
+            # input is (nc) x 32 x 32
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True)
+            # state size. (ndf) x 14 x 14
+        )
+        self.label = nn.Sequential(
+            # input is (num_classes) x 32 x 32
+            nn.Conv2d(num_classes, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True)
+            # state size. (ndf) x 14 x 14
+        )
+        self.main = nn.Sequential(
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 7 x 7
+            nn.Conv2d(ndf * 4, ndf * 8, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            # state size. (1) x 1 x 1
+            nn.Sigmoid()
+        )
+
+    def forward(self, image, label):
+        image = self.image(image)
+        label = self.label(label)
+        incat = torch.cat((image, label), dim=1)
+        return self.main(incat)
+
+class ComplexAutoencoder(nn.Module):
+    def __init__(self):
+        super(ComplexAutoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+    def reverse(self, x):
+        x = self.encoder(x)
+        return x
+    def reconstruction(self, x):
+        x = self.decoder(x)
+        return x
+
+
+class Reverse_Generator(nn.Module):
+    def __init__(self):
+        super(Reverse_Generator, self).__init__()
+        self.fc = nn.Linear(10, 128)
+        self.conv_layers = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=7, stride=1, padding=0),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1),
+            nn.Tanh()
+        )
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(-1, 128, 1, 1)
+        x = self.conv_layers(x)
+        return x
+
 def calculate_test_accuracy(model, data_loader):
     model.eval()
     correct = 0
@@ -41,6 +182,7 @@ def calculate_test_accuracy(model, data_loader):
     print('Accuracy:',accuracy)
 
 def calculate_agreement_accuracy(one_model,two_model,one_dataloader):
+    one_model.cuda()
     one_model.eval()
     two_model.eval()
     correct = 0
