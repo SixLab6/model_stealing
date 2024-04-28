c958ae00d3c24fa16c2ac2be7784c4f54fe551b2
Simon <2053104031@qq.com>
2024-04-28 23:40:27 +1000

    Update train_victim_model.py

--- MNIST/train_victim_model.py
+++ MNIST/train_victim_model.py
@@ -1,1 +1,39 @@
+import torch.optim as optim
+import torchvision
+from utils_mnist import *
+from torch.utils.data import DataLoader
+from torchvision import utils, datasets, transforms
+
+torch.manual_seed(10)
+# load data set mnist(0~9)
+transform = transforms.Compose([
+        transforms.Resize(28),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = DataLoader(testset, batch_size=64, shuffle=True)
+# load model
+model = CNN()
+model.cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+# start training
+for epoch in range(10):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        inputs=inputs.cuda()
+        labels=labels.cuda()
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    print(f'Epoch {epoch+1}, Loss: {running_loss / len(trainloader)}')
+    calculate_test_accuracy(model,testloader)
+torch.save(model.state_dict(), 'victim_mnist_model.pth')
 
